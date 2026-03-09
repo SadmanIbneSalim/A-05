640bf755eac0486718e4c9848c45a9e809a5a8ae
sadmanibnesalim <sadmanibnesalim@gmail.com>
2026-03-09 20:01:49 +0600

    log in js added

---
+++ js/log-in.js
@@ -0,0 +1,18 @@
+
+document.getElementById('log-in-btn').addEventListener('click',function(){
+    const logInUsername=document.getElementById('logIn-username');
+username=logInUsername.value;
+
+const logInPassword=document.getElementById('logIn-password');
+password=logInPassword.value;
+
+if(username==='admin' && password==='admin123'){
+    window.location.assign('/home.html')
+
+
+}
+else{
+    alert('invalid')
+}
+
+})
